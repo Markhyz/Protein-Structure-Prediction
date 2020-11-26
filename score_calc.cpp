835283fc7dda7af95c6b86e099be69ce5fb235ea
Markhyz <felipe.r.marchi@gmail.com>
2020-11-25 21:49:36 -0300

    lel

--- score_calc.cpp
+++ score_calc.cpp
@@ -11,9 +11,14 @@ int main(int argc, char** argv) {
     core::init::init(1, argv2);
     string structure_file_1 = argv[1];
     string structure_file_2 = argv[2];
+    size_t pose_start = argc > 2 ? stoi(argv[2]) : 1;
 
     core::pose::Pose structure_1;
     core::import_pose::pose_from_file(structure_1, structure_file_1, false, core::import_pose::PDB_file);
+
+    if (pose_start - 1 > 0) {
+        structure_1.delete_residue_range_slow(1, pose_start - 1);
+    }
 
     core::pose::Pose structure_2;
     core::import_pose::pose_from_file(structure_2, structure_file_2, false, core::import_pose::PDB_file);
