a9663c01ee628582ee556c1671ded2849c3b0275
Markhyz <felipe.r.marchi@gmail.com>
2021-06-30 17:40:15 -0300

    added mean scripts

---
+++ scripts/hypervolume_mean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in `seq 1 $2`;
+do
+    for j in `seq 1 $3`;
+    do
+        frag_hv=`python3 scripts/fitness_convergence.py $1/$i/algorithm/frag_gen_$j`
+        res_hv=`python3 scripts/fitness_convergence.py $1/$i/algorithm/res_gen_$j`
+        echo $j $frag_hv > $1/$i/algorithm/frag.hv
+        echo $j $res_hv > $1/$i/algorithm/res.hv
+    done
+    python3 scripts/mean_hypervolume.py $1 $2 algorithm/frag.hv > $4/mean_frag.hv
+    python3 scripts/mean_hypervolume.py $1 $2 algorithm/res.hv > $4/mean_res.hv
+done
---
+++ scripts/mean_hypervolume.py
@@ -0,0 +1,25 @@
+import sys
+
+protein_dir = sys.argv[1]
+size = int(sys.argv[2])
+file_path = sys.argv[3]
+
+def read_hypervolume_file(filename):
+    values = []
+    with open(filename) as file:
+        fitness = []
+        for line in file:
+            if line != "":
+                line_values = line.split()
+                values.append(float(line_values[1]))
+    return values
+
+mean_hypervolume = read_hypervolume_file(f'{protein_dir}/1/{file_path}')
+for i in range(2, size + 1):
+    hypervolume = read_hypervolume_file(f'{protein_dir}/{i}/{file_path}')
+    for (index, value) in enumerate(hypervolume):
+        mean_hypervolume[index] += value
+
+for i in range(len(mean_hypervolume)):
+    mean_hypervolume[i] /= size
+    print("{} {:.9f}".format(i + 1, mean_hypervolume[i]))
