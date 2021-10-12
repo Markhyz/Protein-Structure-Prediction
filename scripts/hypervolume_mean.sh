89f2afc7b8fd5adfb01c19cdcf213dce518d5ec8
Markhyz <felipe.r.marchi@gmail.com>
2021-10-12 13:58:28 -0300

    changed scripts

--- scripts/hypervolume_mean.sh
+++ scripts/hypervolume_mean.sh
@@ -2,13 +2,8 @@
 
 for i in `seq 1 $2`;
 do
-    for j in `seq 1 $3`;
-    do
-        frag_hv=`python3 scripts/fitness_convergence.py $1/$i/algorithm/frag_gen_$j`
-        res_hv=`python3 scripts/fitness_convergence.py $1/$i/algorithm/res_gen_$j`
-        echo $j $frag_hv >> $1/$i/algorithm/frag.hv
-        echo $j $res_hv >> $1/$i/algorithm/res.hv
-    done
+    python3 scripts/fitness_convergence.py $1/$i/algorithm/frag_gen_ $3 > $1/$i/algorithm/frag.hv;
+    python3 scripts/fitness_convergence.py $1/$i/algorithm/res_gen_ $3 > $1/$i/algorithm/res.hv;
 done
 
 python3 scripts/mean_hypervolume.py $1 $2 algorithm/frag.hv > $4/mean_frag.hv
--- scripts/scores.sh
+++ scripts/scores.sh
@@ -6,16 +6,20 @@ proteins=(1ab1 1acw 1ail 1aly 1bdd 1crn 1dfn 1enh 1gb1 1hhp 1i6c 1rop 1zdd 2kdl 
 
 for protein in ${proteins[@]};
 do
-     	cat results_predictor/$protein/*/scores/res.ca_rmsd > statistics/predictor/$protein/best_rmsd
-    	cat results_predictor/$protein/*/scores/res.gdtts > statistics/predictor/$protein/best_gdt
-    	cat results_predictor/$protein/*/scores/res.mean_rmsd > statistics/predictor/$protein/mean_rmsd
-    	cat results_predictor/$protein/*/scores/res.mean_gdt > statistics/predictor/$protein/mean_gdt
-   ##for i in `seq 1 20`;
-    #do
-   # 	cat results_predictor/$protein/$i/scores/res.ca_rmsd >> statistics/predictor/best_rmsd
-   # 	cat results_predictor/$protein/$i/scores/res.ca_gdtts >> statistics/predictor/best_gdt
-   # 	cat results_predictor/$protein/$i/scores/res.mean_rmsd >> statistics/predictor/mean_rmsd
-   # 	cat results_predictor/$protein/$i/scores/res.mean_gdt >> statistics/predictor/mean_gdt
-   # done
+      mkdir -p statistics/$2/$protein
+
+     	cat $2/$protein/*/scores/res.ca_rmsd > statistics/predictor/$protein/best.rmsd
+    	cat $2/$protein/*/scores/res.gdtts > statistics/predictor/$protein/best.gdt
+    	cat $2/$protein/*/scores/res.mean_rmsd > statistics/predictor/$protein/mean.rmsd
+    	cat $2/$protein/*/scores/res.mean_gdt > statistics/predictor/$protein/mean.gdt
+
+      cp $2/$protein/1/algorithm/frag_gen_1.norm_fitness statistics/$2/$protein/frag_first_frontier.norm_fitness
+      cp $2/$protein/1/algorithm/frag_gen_$4.norm_fitness statistics/$2/$protein/frag_last_frontier.norm_fitness
+      cp $2/$protein/1/algorithm/res_gen_1.norm_fitness statistics/$2/$protein/res_first_frontier.norm_fitness
+      cp $2/$protein/1/algorithm/res_gen_$4.norm_fitness statistics/$2/$protein/res_last_frontier.norm_fitness
+
+      python3 scripts/mean_diversity.py $2/$protein $3 algorithm/total.diversity > statistics/$2/$protein/mean.diversity
+      
+      ./scripts/hypervolume_mean.sh $2/$protein $3 $4 statistics/$2/$protein
 done
 
