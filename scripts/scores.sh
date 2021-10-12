cbfaa4e7b1eeac47a74bab144819d65f455e2542
Markhyz <felipe.r.marchi@gmail.com>
2021-10-12 14:00:58 -0300

    changed scripts

--- scripts/scores.sh
+++ scripts/scores.sh
@@ -8,18 +8,18 @@ for protein in ${proteins[@]};
 do
       mkdir -p statistics/$2/$protein
 
-     	cat $2/$protein/*/scores/res.ca_rmsd > statistics/predictor/$protein/best.rmsd
-    	cat $2/$protein/*/scores/res.gdtts > statistics/predictor/$protein/best.gdt
-    	cat $2/$protein/*/scores/res.mean_rmsd > statistics/predictor/$protein/mean.rmsd
-    	cat $2/$protein/*/scores/res.mean_gdt > statistics/predictor/$protein/mean.gdt
+     	cat $1/$protein/*/scores/res.ca_rmsd > statistics/predictor/$protein/best.rmsd
+    	cat $1/$protein/*/scores/res.gdtts > statistics/predictor/$protein/best.gdt
+    	cat $1/$protein/*/scores/res.mean_rmsd > statistics/predictor/$protein/mean.rmsd
+    	cat $1/$protein/*/scores/res.mean_gdt > statistics/predictor/$protein/mean.gdt
 
-      cp $2/$protein/1/algorithm/frag_gen_1.norm_fitness statistics/$2/$protein/frag_first_frontier.norm_fitness
-      cp $2/$protein/1/algorithm/frag_gen_$4.norm_fitness statistics/$2/$protein/frag_last_frontier.norm_fitness
-      cp $2/$protein/1/algorithm/res_gen_1.norm_fitness statistics/$2/$protein/res_first_frontier.norm_fitness
-      cp $2/$protein/1/algorithm/res_gen_$4.norm_fitness statistics/$2/$protein/res_last_frontier.norm_fitness
+      cp $1/$protein/1/algorithm/frag_gen_1.norm_fitness statistics/$2/$protein/frag_first_frontier.norm_fitness
+      cp $1/$protein/1/algorithm/frag_gen_$4.norm_fitness statistics/$2/$protein/frag_last_frontier.norm_fitness
+      cp $1/$protein/1/algorithm/res_gen_1.norm_fitness statistics/$2/$protein/res_first_frontier.norm_fitness
+      cp $1/$protein/1/algorithm/res_gen_$4.norm_fitness statistics/$2/$protein/res_last_frontier.norm_fitness
 
-      python3 scripts/mean_diversity.py $2/$protein $3 algorithm/total.diversity > statistics/$2/$protein/mean.diversity
+      python3 scripts/mean_diversity.py $1/$protein $3 algorithm/total.diversity > statistics/$2/$protein/mean.diversity
       
-      ./scripts/hypervolume_mean.sh $2/$protein $3 $4 statistics/$2/$protein
+      ./scripts/hypervolume_mean.sh $1/$protein $3 $4 statistics/$2/$protein
 done
 
